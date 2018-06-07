6eaa16fe4737a5911202d71a64528c0455ea1320
gananggww <gananggww@gmail.com>
2018-06-07 15:05:53 +0700

    syling item list and market status every single list

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -4,24 +4,54 @@ import { connect } from 'react-redux'
 import { cryptoCurrencyList } from '../../redux/action/index_action.jsx'
 
 
-import '../../App.css';
-// {JSON.stringify(Object.values(this.props.cryptoCurrencyList_state.cryptoCurrencyList))}
+import '../../style/content.css';
 
 
 class Content extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color_percent: 'color-red'
+    }
+  }
 
   componentWillMount() {
     this.props.cryptoCurrencyList_dispatch()
   }
 
+  convert_currency(value) {
+    return '$' + value.toLocaleString('es-US')
+  }
+
+  convert_percent(value) {
+    if (true) {
+      // this.setState({color_percent:'color_red'})
+      return value + "%"
+    }
+  }
+
   render() {
     if (this.props.cryptoCurrencyList_state) {
       return (
-        <div className="content">
+        <div className="container">
           {
             this.props.cryptoCurrencyList_state.map(item => {
               return (
-                <div key={item.id}>{item.name}</div>
+                <div className="item">
+                  <div className="name-list">
+                    <div className="img-base-list">
+                      <img src={`https://s2.coinmarketcap.com/static/img/coins/16x16/${item.id}.png`}></img>
+                    </div>
+                    <div key={item.id}>{item.name}</div>
+                  </div>
+                  <div className="status-item">
+                    <div className="status-detail color-one">{this.convert_currency(item.quotes.USD.market_cap)}</div>
+                    <div className="status-detail color-two">{this.convert_currency(item.quotes.USD.price)}</div>
+                    <div className="status-detail color-one">{this.convert_currency(item.quotes.USD.volume_24h)}</div>
+                    <div className="status-detail color-two">{item.circulating_supply}</div>
+                    <div id={item.quotes.USD.percent_change_24h.toString()[0] !== '-' ? 'color-black' : 'color-red'} className="status-detail color-one right-item-detail">{this.convert_percent(item.quotes.USD.percent_change_24h)}</div>
+                  </div>
+                </div>
               )
             })
           }
